74d75dc95ed010d3ffffc36e99b96609d4c16ef2
Yura Shevchuk <yurayu92@gmail.com>
2013-08-20 13:48:01 +0300

    Changes

--- default/settings.py
+++ default/settings.py
@@ -3,5 +3,5 @@ USPS_CONNECTION_SECURE = 'https://secure.shippingapis.com/ShippingAPI.dll'
 USPS_CONNECTION_TEST = 'http://testing.shippingapis.com/ShippingAPITest.dll'
 USPS_CONNECTION_TEST_PRODUCTION = 'http://production.shippingapis.com/ShippingAPITest.dll'
 USPS_CONNECTION_TEST_SECURE = 'https://secure.shippingapis.com/ShippingAPITest.dll'
-USERNAME = "717OSTIN4819"
-PASSWORD = "520KP93ZZ787"
+USERNAME = "***"
+PASSWORD = "***"
